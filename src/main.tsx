29be5adf33d6282104e15d71e030588a2bf9f9d3
Omnistice3222 <om.esthetica@gmail.com>
2026-03-10 13:09:26 +0300

    Исправление ошибок

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = window.location.origin;
+axios.defaults.baseURL = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1' ? 'http://localhost:3000' : window.location.origin;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
